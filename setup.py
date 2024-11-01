3dccb95532486284c92c14379140829af413c0ea
kokodev <github@kokofixcomputers.serv00.net>
2024-11-01 16:16:10 -0700

    1.0.2

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='configurationlib',
-    version='1.0.2',
+    version='1.1.0',
     author='kokodev',
     author_email='koko@kokodev.cc',
     description='A simple configuration manager',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=[],  # Add any dependencies if needed
+    install_requires=[
+        'pyyaml',  # Specify PyYAML as a dependency
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
