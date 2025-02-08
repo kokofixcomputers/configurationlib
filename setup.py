39d19d44ec8aa861d1e4ac973fc8f65b02ac7fed
kokodev <github@kokofixcomputers.serv00.net>
2025-02-08 10:05:18 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='configurationlib',
-    version='1.2.1',
+    version='1.2.2',
     author='kokodev',
     author_email='koko@kokodev.cc',
     description='A simple configuration manager',
